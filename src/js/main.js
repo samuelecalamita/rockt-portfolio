a5ec2527f22e106802ee8b240fc2b0919a7d09b5
orsoblast <samuelecalamita@gmail.com>
2017-06-26 23:53:54 +0200

    toggle menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,20 @@
 var scene = document.getElementById('scene');
 
 var parallax = new Parallax(scene);
+
+var toggleButton = document.getElementsByClassName('menu-toggle')[0];
+var body = document.querySelector('body');
+
+console.log(toggleButton);
+console.log(body);
+
+body.addEventListener('click', function(event) {
+  if( event.target.classList.contains('menu-toggle')) {
+    event.preventDefault();
+    if(this.classList.contains('contact-container-in')) {
+      this.classList.remove('contact-container-in');
+    } else {
+      this.classList.add('contact-container-in');
+    }
+  }
+});
